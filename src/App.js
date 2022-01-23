7c0edeb1bdca6edfb1efeb7600d9e7e42cbc99b3
Mehzabin Sadat Aothoi <mehzabin.sadat.aothoi@g.bracu.ac.bd>
2022-01-23 22:28:03 +0600

    updated appjs

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './pages/Home/Home'
 import Meditation from './pages/meditationPage/Meditation' 
 import GuidedMeditation from './pages/guidedMeditation/GuidedMeditation';
 import UnguidedMeditation from './pages/unguidedMeditation/UnguidedMeditation';
-
+import Diet from './pages/Diet/Diet';
 
 function App() {
   return (
