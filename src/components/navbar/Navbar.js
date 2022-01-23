a289e245f24eb27b043cf65e42faba328583f437
Samin Ahsan <turzo68@gmail.com>
2022-01-23 22:48:36 +0600

    fixed home

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Navbar.css'
+import {Link} from 'react-router-dom'
+
 function Navbar() {
   return <div className='navBarContainer'>
       <div className="navLinks">
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,24 +4,24 @@ import "./Home.css"
 const Home = () => {
     return (
         <div className='homeMain'>
-            <h1>Your guide to health and happiness</h1>
-            <p>Live a healthier, happier, more peaceful life with InnerPeace</p>
+            <h1>Inner Peace</h1>
+            <p>Your guide to health and happiness</p>
             <div className="cards">
-                <div className="homeCard">
-                    <img className='meditation' src="" alt="https://storyset.com/nature" />
+                <div className="homeCard1">
+                    {/* <img className='meditation' src="" alt="https://storyset.com/nature" /> */}
                     <button className='meditate'>Meditate</button>
                 </div>
-                <div className="homeCard">
-                    <img className='bmi' src="" alt="https://storyset.com/data" />
+                <div className="homeCard2">
+                    {/* <img className='bmi' src="" alt="https://storyset.com/data" /> */}
                     <button className='bmi'>Calculate BMI</button>
                 </div>
-                <div className="homeCard">
-                    <img className='exercise' src="" alt="https://storyset.com/workout" />
+                <div className="homeCard3">
+                    {/* <img className='exercise' src="" alt="https://storyset.com/workout" /> */}
                     <button className='exercise'>Exercise</button>
                 </div>
-                <div className="homeCard">
-                    <img className='diet' src="" alt="https://storyset.com/food" />
-                    <button className='diet'>Recepies</button>
+                <div className="homeCard4">
+                    {/* <img className='diet' src="" alt="https://storyset.com/food" /> */}
+                    <button className='diet'>Recipes</button>
                 </div>
             </div>
         </div>
